c0e64b0b05abb88f73ce9b04b2c8e48b879b306e
Mráz Gergő <mrazgergo96@gmail.com>
2024-07-28 14:14:54 +0200

    Custom Exception classes added.

---
+++ ExceLite/Exceptions/NoValidPropertyException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExceLite.Exceptions
+{
+    public class NoValidPropertyException : Exception
+    {
+        public Type Type { get; }
+
+        public NoValidPropertyException(Type type) : base($"The type {type.Name} does not have any public properties.")
+        {
+            Type = type;
+        }
+    }
+}
---
+++ ExceLite/Exceptions/SheetNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExceLite.Exceptions
+{
+    public class SheetNotFoundException : Exception
+    {
+        public string SheetName { get; }
+
+        public SheetNotFoundException(string sheetName) : base($"Sheet not found with name '{sheetName}'")
+        {
+            SheetName = sheetName;
+        }
+    }
+}
